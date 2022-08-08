98dc117e5f63d8145bfd091096bd5fe689d6e800
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-08-08 18:04:00 +0200

    feature: Cleanup main with macro

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
 use anyhow::Result;
-use cmdhub::register_cmd;
 use log::info;
 use rutils::file_logger::setup_logger;
-use std::env;
-use std::process::exit;
 
-fn main() -> Result<()> {
+#[command]
+fn entry() -> Result<()> {
     setup_logger()?;
     info!("starting command");
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 2 && args[1] == "register" {
-        register_cmd()?;
-        exit(0);
-    }
-    Ok(())
 }
