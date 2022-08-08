7bf6f028263243d84d379f1edc09320b49e1b7e8
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2022-08-08 13:33:04 +0200

    fix: Fix arguments check

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use std::process::exit;
 fn main() -> Result<()> {
     setup_logger()?;
     info!("starting command");
-    let args: Vec<_> = env::args().collect();
-    if args.len() == 2 && args[1] == "register".into() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 && args[1] == "register" {
         register_cmd()?;
         exit(0);
     }
